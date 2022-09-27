758fc0a95a7f801e6097b4f4e942588d116f0134
Stéphane Caron <stephane.caron@normalesup.org>
2022-09-27 18:27:16 +0200

    Add unit test for frame

---
+++ tests/test_frame.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2022 Stéphane Caron
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Test frame.
+"""
+
+import unittest
+
+import meshcat
+import meshcat_shapes
+
+
+class TestFrame(unittest.TestCase):
+    def setUp(self):
+        self.visualizer = meshcat.Visualizer().open()
+
+    def test_frame(self):
+        meshcat_shapes.set_frame(self.visualizer["test_frame"])
